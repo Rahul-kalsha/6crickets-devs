0b222478be59df0a5ed6eef471a27401f66d77d8
RahulKalsha <kalsarahul@gmail.com>
2024-10-19 19:57:40 +0530

    Fixed Http request calling issue

--- projects/coding-challenge/src/app/app.config.ts
+++ projects/coding-challenge/src/app/app.config.ts
@@ -5,8 +5,7 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimations(), provideHttpClient(withFetch()), provideHttpClientTesting() ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimations(), provideHttpClient(withFetch())]
 };
