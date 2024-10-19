6fd30e6c536b5abe0d5144cc35ab44a7f1050722
RahulKalsha <kalsarahul@gmail.com>
2024-10-19 15:03:31 +0530

    Project Setup + Implemented Coding - Phase 1 + Added web worker for perfomance improvement

---
+++ projects/coding-challenge/src/app/components/camera-selection/camera-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-camera-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './camera-selection.component.html',
+  styleUrl: './camera-selection.component.scss'
+})
+export class CameraSelectionComponent {
+
+}
